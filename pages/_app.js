8bf14d9814970387bc21c851b640871a8becd178
harshu-pixel <kanishkd05@gmail.com>
2026-04-22 10:15:39 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,5 @@
 import '../styles/globals.css';
-import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <title>🎂 Birthday Creator</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
